b3ac8b5918bb36ef496d54044472c6c674fac919
Luis Capelo <luiscape@gmail.com>
2014-06-26 09:46:41 -0400

    adding the hdx pcodes

--- code/entity.R
+++ code/entity.R
@@ -16,20 +16,25 @@ divipola <- read.xlsx2('data/source/divipola.xlsx', sheetIndex = 1, startRow = 5
 
 message('Creating separate p-code lists.')
 
+iso3 <- 'COL-'
+
 # for departamentos
 admin2_list <- data.frame(divipola$Código.Departamento, divipola$Nombre.Departamento)
 admin2_list <- unique(admin2_list)
 names(admin2_list) <- c('admin2', 'admin2_name')
+admin2_list$hdx_pcode <- paste0(iso3, admin2_list$admin2)
 
 # for municipios
 admin3_list <- data.frame(divipola$Código.Municipio, divipola$Nombre.Municipio)
 admin3_list <- unique(admin3_list)
 names(admin3_list) <- c('admin3', 'admin3_name')
+admin3_list$hdx_pcode <- paste0(iso3, admin3_list$admin3)
 
 # for centros poblados
 admin4_list <- data.frame(divipola$Código.Centro.Poblado, divipola$Nombre.Centro.Poblado)
 admin4_list <- unique(admin4_list)
 names(admin3_list) <- c('admin4', 'admin4_name')
+admin4_list$hdx_pcode <- paste0(iso3, admin4_list$admin4)
 
 # summary table
 admin2 <- nrow(admin2_list)
@@ -38,10 +43,13 @@ admin4 <- nrow(admin4_list)
 total <- admin2 + admin3 + admin4
 summary <- data.frame(admin2, admin3, admin4, total)
 
+message('Storing CSV files...')
 # storing CSV
 # Notice that the metropolitan area is ignored from the files.
-write.csv(divilopa, 'data/col_admin_all.csv', row.names = F)
+write.csv(divipola, 'data/col_admin_all.csv', row.names = F)
 write.csv(admin2_list, 'data/col_admin2.csv', row.names = F)
 write.csv(admin3_list, 'data/col_admin3.csv', row.names = F)
 write.csv(admin4_list, 'data/col_admin4.csv', row.names = F)
 write.csv(summary, 'summary_table.csv', row.names = F)
+
+message('Done.')
